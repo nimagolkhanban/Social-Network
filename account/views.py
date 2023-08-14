c60fafecaec4ccb64d5d386aba8e6659cdae1dcd
nimagolkhanban <ningoban@gmail.com>
2023-08-14 19:38:24 +0330

    make registration works better

--- account/views.py
+++ account/views.py
@@ -4,21 +4,28 @@ from django.views import View
 from .forms import UserRegistrationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+
+
+
 class RegisterView(View):
+    form_class = UserRegistrationForm
+
+
     def get(self, request):
-        form = UserRegistrationForm()
+        form = self.form_class()
         return render(request, "account/register.html" , {"form":form})
 
+
+
     def post(self, request):
-        form = UserRegistrationForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(cd["username"] , cd["email"], cd["password"])
             messages.success(request,"you register succesfully", "success")
             return redirect("home:home")
         else :
-            return render(request, "account/register.html")
-
+            return render(request, "account/register.html", {"form": form})
 
 
 
