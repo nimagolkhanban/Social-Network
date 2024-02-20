acaebc44585826de2f24c0952e7b555cb5261eda
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-21 00:15:16 +0330

    adding account app

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views import View
 
 
-def home(request):
-    return render(request, "home/index.html")
+class HomeView(View):
+
+    def get(self, request):
+        return render(request,"home/index.html")
+
+    def post(self, request):
+        return render(request,"home/index.html")
 
