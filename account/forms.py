8ff1f345e870605b4e717f3da768573aece526c8
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-21 18:41:14 +0330

    logout login and dispatch for all

--- account/forms.py
+++ account/forms.py
@@ -6,8 +6,10 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    password1 = forms.CharField(label="password", widget=forms.PasswordInput(attrs={'placeholder': 'your password', 'class': 'form-control'}))
-    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput(attrs={'placeholder':'cnfirm password', 'class': 'form-control'}))
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput(
+        attrs={'placeholder': 'your password', 'class': 'form-control'}))
+    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput(
+        attrs={'placeholder': 'cnfirm password', 'class': 'form-control'}))
 
     def clean_email(self):
         email = self.cleaned_data["email"]
@@ -22,3 +24,9 @@ class UserRegistrationForm(forms.Form):
         if p1 and p2 and p1 != p2 :
             raise ValidationError("password must match")
 
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="password", widget=forms.PasswordInput(
+        attrs={'placeholder': 'your password', 'class': 'form-control'}))
