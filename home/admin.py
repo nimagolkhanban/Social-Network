2b4bc6991663699259247997d4d286b9b3678b4e
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-22 12:27:11 +0330

    post admin panel deno

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "slug", "updated"]
+    search_fields = ['slug']
+    prepopulated_fields = {'slug': ('body',)}
+    list_filter = ['updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(Post, PostAdmin)
