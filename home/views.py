94e99da5ac9f4b1dec81fb98a8b5aafdde89d1f7
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-22 14:28:03 +0330

    impelimen a logic for profile and showing all post of a user in profile

--- home/views.py
+++ home/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .models import Post
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request,"home/index.html")
+        post = Post.objects.all()
+        return render(request,"home/index.html", {"posts":post})
 
     def post(self, request):
         return render(request,"home/index.html")
 
+
+class PostDetailView(View):
+
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        return render(request, "home/detail.html", {"post": post})
