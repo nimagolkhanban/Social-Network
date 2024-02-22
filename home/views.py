daa06cc058d822d9b6a4a7b8ea33c0a40b577dc8
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-23 00:41:10 +0330

    add delete bottom for post that own by current user

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Post
+
 
 class HomeView(View):
 
@@ -18,3 +21,14 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, "home/detail.html", {"post": post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, "your post has been deleted", "success")
+        else:
+            messages.error(request, "this in not your post", "danger")
+        return redirect("home:home")
