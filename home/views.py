7c1589edb9ddbe2ea77e39b6cae1a5801a8b929c
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-23 03:41:49 +0330

    an important change (adding setup method for better query that will be more db efficient) ,edit slug part

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.utils.text import slugify
 from django.views import View
 from .models import Post
 from .forms import PostUpdateForm
@@ -35,22 +36,31 @@ class PostDeleteView(LoginRequiredMixin, View):
 
 
 class PostUpdateView(LoginRequiredMixin, View):
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs["post_id"])
+        post = self.post_instance
         if post.user.id != request.user.id:
             messages.error(request, "this post is not yours so you can't edit it", "danger")
             return redirect("home:home")
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = PostUpdateForm(instance=post)
         return render(request, "home/update.html", {"form": form})
 
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = PostUpdateForm(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data["body"][:30])
+            new_post.save()
+            messages.success(request, "your post has been changed", "success")
+            return redirect('home:post_detail', post.id, post.slug)
 
-    def post(self, request, post_id):
-
-        pass
 
 
