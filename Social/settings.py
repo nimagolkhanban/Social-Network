a5a03b6d06775de4014f8dff89a993b4399c5547
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-02-24 00:56:24 +0330

    all the 4 step for a class base generic view for reset password with gmail

--- Social/settings.py
+++ Social/settings.py
@@ -131,7 +131,7 @@ AUTHENTICATION_BACKENDS = [
     'account.authenticate.EmailBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ningoban@gmail.com'
